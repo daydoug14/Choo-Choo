69b5ca7a982131a079554c62f54a2725fbe8a166
daydoug14 <daydoug14@gmail.com>
2018-06-28 18:08:07 -0400

    up

---
+++ ChooChoo/assets/javascript/train.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyALXpd7TtM2gDlN5sw_UYenRPJ3__GHJDA",
+    authDomain: "doug2-d3ecd.firebaseapp.com",
+    databaseURL: "https://doug2-d3ecd.firebaseio.com",
+    projectId: "doug2-d3ecd",
+    storageBucket: "",
+    messagingSenderId: "983468566987"
+  };
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
